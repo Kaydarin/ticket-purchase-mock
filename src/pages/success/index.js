76e421a846f01448e4d523308b6a4f8592de256b
Kaydarin <zim50_killerz@yahoo.com>
2022-07-24 17:01:03 +0800

    Added success payment page.

---
+++ src/pages/success/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+    Flex,
+    Box,
+    Stack,
+    Icon
+} from '@chakra-ui/react';
+import { FaRegCheckCircle } from "react-icons/fa";
+
+function Success() {
+    return (
+        <Flex justifyContent='center'>
+            <Box borderWidth='1px' borderRadius='lg' overflow='hidden' p='5' borderColor='#E2E8F0' width='50%'>
+                <Stack spacing={4} alignItems='center'>
+                    <div style={{
+                        fontSize: 28,
+                        fontWeight: 700
+                    }}>Payment Success!</div>
+                    <Icon as={FaRegCheckCircle} boxSize={40} color='teal' />
+                    <div>
+                        We will send the tickets to your email along with the receipt.
+                    </div>
+                </Stack>
+            </Box>
+        </Flex>
+    );
+}
+
+export default Success;
